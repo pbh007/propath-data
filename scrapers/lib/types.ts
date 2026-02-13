f4677332b84cfeab2ce6d3b56c6a68dae103a0b1
pbh007 <pbh007@optonline.net>
2026-02-13 20:49:19 +1300

    Create types.ts

---
+++ scrapers/lib/types.ts
@@ -0,0 +1,16 @@
+export type ProPathEvent = {
+  id?: string;
+  tour?: string;
+  gender?: string;
+  type?: string;
+  stage?: string;
+  title?: string;
+  start?: string | null;
+  end?: string | null;
+  city?: string;
+  state_country?: string;
+  tourUrl?: string;
+  signupUrl?: string;
+  mondayUrl?: string;
+  mondayDate?: string | null;
+};
