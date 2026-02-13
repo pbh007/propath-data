d8570adfc15a00929d6bd1eb6c05200029ee44ec
pbh007 <pbh007@optonline.net>
2026-02-13 19:36:30 +1300

    Refactor import statements in json_api.ts
    Updated import paths in json_api.ts to remove file extensions.

--- scrapers/connectors/json_api.ts
+++ scrapers/connectors/json_api.ts
@@ -1,5 +1,6 @@
-import type { ProPathEvent } from "../lib/types.js";
-import { coerceISO } from "../lib/normalize.js";
+import type { ProPathEvent } from "../lib/types";
+import { coerceISO } from "../lib/normalize";
+
 
 type Source = {
   url: string;
